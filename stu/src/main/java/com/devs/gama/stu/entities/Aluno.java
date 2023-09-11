4d1a662c6eedd796102961369922456e94053074
lzzgabriel <96301579+lzzgabriel@users.noreply.github.com>
2023-09-11 14:51:48 -0300

    :construction: In progress: entities
    - Professor & aluno

---
+++ stu/src/main/java/com/devs/gama/stu/entities/Aluno.java
@@ -0,0 +1,41 @@
+package com.devs.gama.stu.entities;
+
+public class Aluno {
+	private Integer id;
+	private String nome;
+	private String email;
+	private String celular;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+}
