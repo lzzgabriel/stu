7af4b7e179fb199cecdbe4ce2d18a7496cb0c5df
lzzgabriel <96301579+lzzgabriel@users.noreply.github.com>
2023-09-13 17:15:29 -0300

    :construction: In progress: entities
    - Add + FormaPagamento
    - Add + Mensalidade

---
+++ stu/src/main/java/com/devs/gama/stu/entities/Mensalidade.java
@@ -0,0 +1,78 @@
+package com.devs.gama.stu.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Mensalidade {
+	
+	private Aluno aluno;
+	private LocalDate mensalidade;
+	private LocalDate proximoVencimento;
+	private Status status;
+	private BigDecimal valor;
+	private FormaPagamento formaPagamento;
+	private LocalDateTime momentoPagamento;
+	
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	public LocalDate getMensalidade() {
+		return mensalidade;
+	}
+
+	public void setMensalidade(LocalDate mensalidade) {
+		this.mensalidade = mensalidade;
+	}
+
+	public LocalDate getProximoVencimento() {
+		return proximoVencimento;
+	}
+
+	public void setProximoVencimento(LocalDate proximoVencimento) {
+		this.proximoVencimento = proximoVencimento;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public FormaPagamento getFormaPagamento() {
+		return formaPagamento;
+	}
+
+	public void setFormaPagamento(FormaPagamento formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+
+	public LocalDateTime getMomentoPagamento() {
+		return momentoPagamento;
+	}
+
+	public void setMomentoPagamento(LocalDateTime momentoPagamento) {
+		this.momentoPagamento = momentoPagamento;
+	}
+
+
+
+	public static enum Status {
+		EM_ABERTO, ATRASADA
+	}
+}
