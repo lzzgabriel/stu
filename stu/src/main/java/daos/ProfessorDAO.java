a2e159b7b3651c88ae08e81ce9c747c45ba4c320
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2023-09-13 21:55:06 -0300

    :construction: In progress: daos

--- stu/src/main/java/daos/ProfessorDAO.java
+++ stu/src/main/java/daos/ProfessorDAO.java
@@ -2,8 +2,11 @@ package daos;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,15 +18,74 @@ public class ProfessorDAO implements DAO<Professor> {
 	private String tableName = "professor";
 
 	@Override
-	public void save(Professor t) {
+	public void save(Professor t) throws SQLException {
+		if (findById(t.getId()) == null) {
+			try (Connection conn = App.getDataSource().getConnection()) {
+//				PreparedStatement ps = conn.prepareStatement(SqlUtils.montarProcedure("cadastrar_professor", 3, 0)); // Procedure
+//				int parametro = 1;
+//				ps.setString(parametro++, t.getNome());
+//				ps.setString(parametro++, t.getEmail());
+//				ps.setString(parametro++, t.getSenha());
+//				ps.execute();
+				String sql = "INSERT INTO " + tableName + " (id, nome, email, senha) VALUES (?, ?, ?, ?)";
+				PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+				int parametro = 1;
+				preparedStatement.setNull(parametro++, Types.INTEGER);
+				preparedStatement.setString(parametro++, t.getNome());
+				preparedStatement.setString(parametro++, t.getEmail());
+				preparedStatement.setString(parametro++, t.getSenha());
+				int linhasAfetadas = preparedStatement.executeUpdate();
+				if (linhasAfetadas > 0) {
+					ResultSet chavesGeradas = preparedStatement.getGeneratedKeys();
+					if (chavesGeradas.next()) {
+						int novoId = chavesGeradas.getInt(1);
+						System.out.println(novoId);
+						// registro inserido
+					} else {
+						// nenhum registro inserido
+					}
+				}
+			}
+		} else {
+			edit(t);
+		}
 	}
 
 	@Override
-	public void edit(Professor t) {
+	public void edit(Professor t) throws SQLException {
+		try (Connection conn = App.getDataSource().getConnection()) {
+			String sql = "UPDATE " + tableName + " SET nome = ?, email = ?, senha = ? WHERE id = ?";
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			int parametro = 1;
+			preparedStatement.setString(parametro++, t.getNome());
+			preparedStatement.setString(parametro++, t.getEmail());
+			preparedStatement.setString(parametro++, t.getSenha());
+			preparedStatement.setInt(parametro++, t.getId());
+			int linhasAfetadas = preparedStatement.executeUpdate();
+			if (linhasAfetadas > 0) {
+				// atualizou
+			} else {
+				// nenhum registro atualizado
+			}
+		}
 	}
 
 	@Override
-	public void delete(Professor t) {
+	public void delete(Professor t) throws SQLException {
+
+		try (Connection conn = App.getDataSource().getConnection()) {
+			String sql = "DELETE FROM " + tableName + " WHERE id = ?";
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			int parametro = 1;
+			preparedStatement.setInt(parametro++, t.getId());
+			int linhasAfetadas = preparedStatement.executeUpdate();
+			if (linhasAfetadas > 0) {
+				// registro excluido
+			} else {
+				// nenhum registro deletado
+			}
+		}
+
 	}
 
 	@Override
@@ -34,9 +96,9 @@ public class ProfessorDAO implements DAO<Professor> {
 		String sql = "select * from " + tableName;
 
 		try (Connection conn = App.getDataSource().getConnection()) {
-			
+
 			CallableStatement callableStatement = conn.prepareCall(sql);
-			
+
 			try (ResultSet resultSet = callableStatement.executeQuery()) {
 				while (resultSet.next()) {
 					returnList.add(fetch(resultSet));
@@ -54,7 +116,18 @@ public class ProfessorDAO implements DAO<Professor> {
 
 	@Override
 	public Professor findById(int id) throws SQLException {
-		return null;
+		Professor professor = null;
+		String sql = "select * from " + tableName + " where id = ?";
+		try (Connection conn = App.getDataSource().getConnection()) {
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			int parametro = 1;
+			preparedStatement.setInt(parametro++, id);
+			ResultSet rs = preparedStatement.executeQuery();
+			if (rs.next()) {
+				professor = fetch(rs);
+			}
+		}
+		return professor;
 	}
 
 	@Override
