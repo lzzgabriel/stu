fe4f55b4b8eb57b7296bf5cd1a47121893e031cd
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2023-09-14 09:59:09 -0300

    :bug: fix: SqlUtils

--- stu/src/main/java/utils/SqlUtils.java
+++ stu/src/main/java/utils/SqlUtils.java
@@ -2,11 +2,19 @@ package utils;
 
 public class SqlUtils {
 
-	public static String montarProcedure(String nomeProcedure, int parametrosEntrada, int parametrosSaida) {
+	/**
+	 * Método para montar a estrutura de uma procedure
+	 * 
+	 * @param nomeProcedure     String Texto que será colocado no nome da procedure
+	 * @param parametrosEntrada Total de parametros de entrada
+	 * @param parametrosRetorno Total de parametros de retorno
+	 * @return String o texto repetido
+	 */
+	public static String montarProcedure(String nomeProcedure, int parametrosEntrada, int parametrosRetorno) {
 		StringBuilder procedure = new StringBuilder("{call " + nomeProcedure + "(");
-		for (int i = 0; i < parametrosEntrada + parametrosSaida; i++) {
-			procedure.append("?, ");
-		}
+		procedure.append(parametrosEntrada > 0 ? "?" + StringUtils.stringReplicate(", ?", parametrosEntrada - 1) : "");
+		procedure
+				.append(parametrosRetorno > 0 ? ", ?" + StringUtils.stringReplicate(", ?", parametrosRetorno - 1) : "");
 		procedure.append(")}");
 		return procedure.toString();
 	}
