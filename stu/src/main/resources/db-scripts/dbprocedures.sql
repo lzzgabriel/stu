191f3034eb925530536626a9a361a9f398209390
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2023-09-22 08:38:41 -0300

    :bug: fix: scripts sql

--- stu/src/main/resources/db-scripts/dbprocedures.sql
+++ stu/src/main/resources/db-scripts/dbprocedures.sql
@@ -1,17 +1,25 @@
 -- Cadastrar Aluno
-CREATE DEFINER=`stustd`@`localhost` PROCEDURE `CADASTRAR_ALUNO`(
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CADASTRAR_ALUNO`(
 OUT retId INT,
 IN a_id INT,
 IN a_nome varchar(100),
 IN a_email varchar(100),
-IN a_celular varchar(11))
+IN a_celular varchar(11),
+IN p_id INT)
 BEGIN
 DECLARE novo_id INT;
-IF a_id IS NULL THEN
+DECLARE resultAssoc INT;
+SET resultAssoc = 0;
+IF a_id IS NULL THEN 
 	INSERT INTO stu.aluno(nome, email, celular)
 	VALUES (a_nome, a_email, a_celular); -- inserir tabela aluno
 	SET novo_id = LAST_INSERT_ID(); -- recuperar novo id
-	SET retId = novo_id;
+    CALL stu.ASSOCIAR_ALUNO_PROFESSOR(resultAssoc, p_id, novo_id); -- chamar procedure de associação
+    IF resultAssoc = 1 THEN
+		SET retId = novo_id;
+	ELSE
+		SET retId = 0;
+	END IF;
 ELSE
 	SET retId = 0;
 	IF EXISTS(SELECT 1 FROM view_aluno va WHERE va.id = a_id) THEN
@@ -22,27 +30,8 @@ ELSE
 END IF;
 END
 
-CREATE DEFINER=`stustd`@`localhost` PROCEDURE `DELETE_PROFESSOR`(OUT result INT, IN p_id INT)
-BEGIN
-/*DECLARE EXIT HANDLER FOR SQLEXCEPTION
-BEGIN
-	ROLLBACK;
-    SELECT 'An error ocurred' as msgError;
-END;*/
-
-IF p_id IS NULL THEN
-	SET result = 0;
-ELSE
-	SET result = 0;
-	 IF EXISTS (SELECT 1 FROM view_professor vp WHERE vp.id = p_id) THEN
-		DELETE FROM professor p WHERE p.id = p_id;
-		 SET result = 1;
-	 END IF;
-END IF;
-END
-
 -- Cadastrar professor
-CREATE DEFINER=`root`@`localhost` PROCEDURE `cadastrar_professor`(OUT retId INT,
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CADASTRAR_PROFESSOR`(OUT retId INT,
          IN p_id INT,
          IN p_nome VARCHAR(100),
          IN p_email VARCHAR(100),
@@ -54,30 +43,15 @@ IF p_id IS NULL THEN
 	SET retId = LAST_INSERT_ID();
 ELSE
 	SET retId = 0;
-	IF EXISTS (SELECT 1 FROM view_professor vp WHERE vp.id = p_id) THEN
+	IF EXISTS (SELECT 1 FROM view_professor vp WHERE vp.id = p_id AND vp.ativo = 1) THEN
 		UPDATE stu.professor p SET p.nome = p_nome, p.email = p_email WHERE p.id = p_id;
 		SET retId = 1;
     END IF;
 END IF;
 END
 
-
--- Deletar professor
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_professor`(OUT result INT, IN p_id INT)
-BEGIN
-IF p_id IS NULL THEN
-	SET result = 0;
-ELSE
-	SET result = 0;
-	IF EXISTS (SELECT 1 FROM view_professor vp WHERE vp.id = p_id) THEN
-		DELETE FROM professor p WHERE p.id = p_id;
-		SET result = 1;
-	END IF;
-END IF;
-END
-
 -- Alterar senha professor
-CREATE DEFINER=`root`@`localhost` PROCEDURE `alterar_senha_professor`(OUT retId INT, IN id_professor INT, 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ALTERAR_SENHA_PROFESSOR`(OUT retId INT, IN id_professor INT, 
 	IN senhaAtual VARCHAR(100), IN senhaDestino VARCHAR(100))
 BEGIN
 SET retId = 0;
@@ -90,7 +64,7 @@ END IF;
 END
 
 -- Associar aluno a professor
-CREATE DEFINER=`stustd`@`localhost` PROCEDURE `ASSOCIAR_ALUNO_PROFESSOR`(IN id_p INT, IN id_a INT)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ASSOCIAR_ALUNO_PROFESSOR`(IN id_p INT, IN id_a INT)
 BEGIN
 IF id_p IS NOT NULL AND id_a IS NOT NULL THEN
 	IF EXISTS (SELECT 1 FROM view_professor vp WHERE vp.id = id_p) AND EXISTS
@@ -101,7 +75,7 @@ END IF;
 END
 
 -- Cadastrar forma pagamento
-CREATE DEFINER=`stustd`@`localhost` PROCEDURE `cadastrar_forma_pagamento`(OUT retId INT,
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CADASTRAR_FORMA_PAGAMENTO`(OUT retId INT,
          IN f_id INT,
          IN f_descricao VARCHAR(100))
 BEGIN
@@ -117,8 +91,8 @@ ELSE
 END IF;
 END
 
--- Deletar forma pagamento
-CREATE DEFINER=`stustd`@`localhost` PROCEDURE `deletar_forma_pagamento`(OUT result INT, IN f_id INT)
+-- Deletar forma pagamento (pode lançar exceção, não corrigido)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DELETAR_FORMA_PAGAMENTO`(OUT result INT, IN f_id INT)
 BEGIN
 IF f_id IS NULL THEN
 	SET result = 0;
@@ -128,5 +102,39 @@ ELSE
 		DELETE FROM forma_pagamento f WHERE f.id = f_id;
 		SET result = 1;
 	END IF;
+END IF;
+END
+
+-- Deletar aluno (pode lançar exceção, não corrigido)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DELETE_ALUNO`(OUT retId INT, IN a_id INT)
+BEGIN
+IF a_id IS NULL THEN
+	SET retId = 0;
+ELSE
+	SET retId = 0;
+	IF EXISTS (SELECT 1 FROM view_aluno va WHERE va.id = a_id) THEN
+		DELETE FROM aluno a WHERE a.id = a_id;
+		SET retId = 1;
+	END IF;
+END IF;
+END
+
+-- Deletar professor (pode lançar exceção, não corrigido)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DELETE_PROFESSOR`(OUT result INT, IN p_id INT)
+BEGIN
+/*DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+	ROLLBACK;
+    SELECT 'An error ocurred' as msgError;
+END;*/
+
+IF p_id IS NULL THEN
+	SET result = 0;
+ELSE
+	SET result = 0;
+	 IF EXISTS (SELECT 1 FROM view_professor vp WHERE vp.id = p_id) THEN
+		DELETE FROM professor p WHERE p.id = p_id;
+		 SET result = 1;
+	 END IF;
 END IF;
 END
